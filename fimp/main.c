c4231d45dfa57cf2ba25185b3b066d27a5dac675
Alessandro Bellucci <Home@MacBook-Pro-di-Mac-Home.local>
2013-11-11 10:03:17 +0100

    First commit.

---
+++ fimp/main.c
@@ -0,0 +1,11 @@
+/*
+* Copyright (C) 2003 Map Bureau - http://www.mapbureau.com
+*
+* This package is Free Software released under the GNU Lesser General Public License (LGPL)
+*
+* See license.html at the top of this package for the full license terms.
+*
+*/
+
+// Code comments temporarily excised, pending review
+
