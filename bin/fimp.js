655b0713ba06feace28a636380b02770d371926c
Alessandro Bellucci <alessandro.bellucci@3e.eu>
2013-11-25 12:13:02 +0100

    Varible declarations and Functions

--- bin/fimp.js
+++ bin/fimp.js
@@ -1,1 +1,1 @@
-var /*xsd:string*/ k;var /*xsd:int*/ l=0;k='test';if(k==='test') {k='A';} else {l=2;}/*xsd:boolean*/ isObject(xsd:int){<unknown Xob type>: rdfs:Resource m=0;x=8;<unknown Xob type>: fabl:Xreturn }
+var /*xsd:string*/ k;var /*xsd:int*/ l=0;k='test';if(k==='test') {k='A';} else {l=2;}function /*xsd:boolean*/ isObject(xsd:int){<unknown Xob type>: rdfs:Resource m=0;x=8;<unknown Xob type>: fabl:Xreturn }
